029a43a3d15f0f30afe81c81947c459065a9586d
Lewis Ho <lewishoch@gmail.com>
2016-10-26 13:42:40 +0800

    fix

--- Customer/src/test/Test1.java
+++ Customer/src/test/Test1.java
@@ -1,23 +1,18 @@
 package test;
 
-<<<<<<< HEAD:Customer/src/test/Test.java
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import po.Customer;
 import service.CustomerManager;
 
-public class Test {
 
-	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		CustomerManager um=context.getBean(CustomerManager.class);
-=======
-import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -31,19 +26,23 @@ public class Test1 {
 	{
 		ApplicationContext context  = new ClassPathXmlApplicationContext("applicationContext.xml");
 		AdvertisementManager m = context.getBean(AdvertisementManager.class);
+		
 		m.findlastestAdv(5);
 		
->>>>>>> Lewis:Customer/src/test/Test1.java
-		
-		Customer c = new Customer();
-		c.setName("hong");
-		c.setPsd("1234");
-		c.setLastModDt(new Date());
-		List<String> addr = new ArrayList<String>();
-			addr.add("address_1");
-			addr.add("address_2");
-			addr.add("address_3");
-		c.setAddress(addr);
-		um.addUser(c);
+//		ApplicationContext context  = new ClassPathXmlApplicationContext("applicationContext.xml");
+//		AdvertisementManager m = context.getBean(AdvertisementManager.class);
+//		m.findlastestAdv(5);
+//		
+//		
+//		Customer c = new Customer();
+//		c.setName("hong");
+//		c.setPsd("1234");
+//		c.setLastModDt(new Date());
+//		List<String> addr = new ArrayList<String>();
+//			addr.add("address_1");
+//			addr.add("address_2");
+//			addr.add("address_3");
+//		c.setAddress(addr);
+//		um.addUser(c);
 	}
 }
