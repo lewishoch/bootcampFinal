4a610f140063deb45145d010445456302390b191
cheungwaihong <w.h.cheung@oocl.com>
2016-10-26 14:58:25 +0800

    merchaent dao, service

---
+++ Customer/src/dao/impl/MerchantImpl.java
@@ -0,0 +1,38 @@
+package dao.impl;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import po.Merchant;
+import dao.MerchantDao;
+
+public class MerchantImpl implements MerchantDao {
+	@PersistenceContext(name="ds")
+	private EntityManager em;
+	
+	@Override
+	public Merchant findMerchant(String id) {
+		String jpql="select m from Merchant m where mid =:mid ";
+		Query q = em.createQuery(jpql);
+		q.setParameter("mid", id);
+		List<Merchant> merchants = q.getResultList();
+		if(merchants.get(0)!=null){
+			return merchants.get(0);
+		} else{
+		return null;
+		}
+		
+	}
+
+	@Override
+	public List<Merchant> findAllMerchant() {
+		String jpql="select m from Merchant m";
+		Query q = em.createQuery(jpql);
+		List<Merchant> merchants = q.getResultList();
+		return merchants;
+	}
+
+}
