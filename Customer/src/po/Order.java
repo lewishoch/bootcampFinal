2bce980e8a10f9d2933428d7440f1ec93c9fb820
Clara Lo <claralo7718@gmail.com>
2016-10-26 10:29:57 +0800

    po edit(2)

--- Customer/src/po/Order.java
+++ Customer/src/po/Order.java
@@ -1,21 +1,37 @@
 package po;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name="order")
 public class Order {
-	@Column(nullable=false)
+	@Id
+	@GenericGenerator(name="oid",strategy="uuid")
+	@GeneratedValue(generator="oid")
+	private String oId;
+	
+	@OneToOne(mappedBy="cId",cascade =CascadeType.PERSIST)
 	private Customer customer;
 	
-	@Column(nullable=false)
+	@OneToOne(mappedBy="mId",cascade =CascadeType.PERSIST)
 	private Merchant merchant;
 	
-	private List<OrderedDish> dishes;
+	@OneToMany(mappedBy="odId",cascade={CascadeType.PERSIST,CascadeType.REMOVE},fetch=FetchType.EAGER)
+	private Set<OrderedDish> dishes = new HashSet<OrderedDish>();
 	
 	@Column(nullable=false)
 	private Integer status;
---
+++ Customer/src/po/ShopInfo.java
@@ -0,0 +1,53 @@
+package po;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class ShopInfo {
+	@Column(nullable=false)
+	private String sName;
+	
+	@Column(nullable=false)
+	private String sAddr;
+	
+	@Column(nullable=false)
+	private String sCat;
+	
+	@Column(nullable=false)
+	private String sTel;
+	
+	@Column(nullable=false)
+	private String sLogoPath;
+	
+	public String getsName() {
+		return sName;
+	}
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+	public String getsAddr() {
+		return sAddr;
+	}
+	public void setsAddr(String sAddr) {
+		this.sAddr = sAddr;
+	}
+	public String getsCat() {
+		return sCat;
+	}
+	public void setsCat(String sCat) {
+		this.sCat = sCat;
+	}
+	public String getsTel() {
+		return sTel;
+	}
+	public void setsTel(String sTel) {
+		this.sTel = sTel;
+	}
+	public String getsLogoPath() {
+		return sLogoPath;
+	}
+	public void setsLogoPath(String sLogoPath) {
+		this.sLogoPath = sLogoPath;
+	}
+}
