5acb68ca438a763ce9d1141b3ae10344514495c3
Lewis Ho <lewishoch@gmail.com>
2016-10-26 13:37:43 +0800

    add test

--- Customer/src/test/Test1.java
+++ Customer/src/test/Test1.java
@@ -1,13 +1,20 @@
 package test;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import service.AdvertisementManager;
 
-public class Test {
-	public static void main(String[] args)
+
+public class Test1 {
+	
+	@Test
+	public void asdf()
 	{
 		ApplicationContext context  = new ClassPathXmlApplicationContext("applicationContext.xml");
+		AdvertisementManager m = context.getBean(AdvertisementManager.class);
+		m.findlastestAdv(5);
 		
 		
 	}
