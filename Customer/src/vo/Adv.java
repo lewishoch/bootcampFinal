1ffd9c4bba5feacab695b86703780bf30118e2a2
Lewis Ho <lewishoch@gmail.com>
2016-10-28 00:36:58 +0800

    adv

---
+++ Customer/src/vo/Adv.java
@@ -0,0 +1,20 @@
+package vo;
+
+public class Adv {
+	private String mid;
+	private String shopLogoName;
+	
+	public String getMid() {
+		return mid;
+	}
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+	public String getShopLogoName() {
+		return shopLogoName;
+	}
+	public void setShopLogoName(String shopLogoName) {
+		this.shopLogoName = shopLogoName;
+	}
+	
+}
